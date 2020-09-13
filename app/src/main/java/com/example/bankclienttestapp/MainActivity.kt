af983a60da1c419a24d21cbd6739c0b0490b9a6f
hornuglan <rita@nextmail.ru>
2020-09-13 09:31:36 +0300

    Feature: toolbar in main activity, transactions recycler view scrollbar

--- app/src/main/java/com/example/bankclienttestapp/MainActivity.kt
+++ app/src/main/java/com/example/bankclienttestapp/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.bankclienttestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.bankclienttestapp.ui.main.MainFragment
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        toolbar = findViewById(R.id.toolbar_main_fragment)
+        setActionBar(toolbar)
         navController = Navigation.findNavController(this, R.id.nav_host)
     }
 }
